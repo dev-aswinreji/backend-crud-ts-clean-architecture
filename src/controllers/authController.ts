b9c7ffa0785ccfea2fc96cdd7e4a901f889d3965
Aswin Reji <aswinreji.004@gmail.com>
2024-09-25 18:41:15 +0000

    feat: Handle error and add interface for the user login

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,7 +1,19 @@
 import { Request, Response } from 'express';
+import { UserLogin } from '../interfaces/user';
 
 class AuthController {
     async login(req:Request,res:Response):Promise<void>{
-        const {email,password} = req.body 
+        const {email,password} = req.body as UserLogin
+
+        try {
+
+        } catch (error) {
+            console.log(error,"Auth Controller error");
+            let errorMessage = "Auth Controller Error"
+            if(error instanceof Error){
+                errorMessage = error.message
+            }
+            res.status(401).json({error:errorMessage})
+        }
     }
 }
