56cc4a99082df6caf2b72ed54568c1d2a3a94ee6
Aswin Reji <aswinreji.004@gmail.com>
2024-09-25 07:03:29 +0000

    feat: UserController fully complemented added simple crud app like adding getting retrieving all user data need to work with postman to understood the whole thing

--- src/controllers/userControllers.ts
+++ src/controllers/userControllers.ts
@@ -1,6 +1,7 @@
 
 import { Request, Response } from 'express';
 import userService from '../services/userService';
+import { CreateUserDTO } from '../interfaces/user';
 
 
 class UserController {
@@ -10,22 +11,36 @@ class UserController {
             res.status(200).json(users)
         } catch (error) {
             console.log(error, "Error in usercontroller getAllUsers");
-            res.status(500).json({error:"Internal Server Error"})
+            res.status(500).json({ error: "Internal Server Error" })
         }
     }
 
-    async getUserById(req:Request,res:Response): Promise<void>{
+    async getUserById(req: Request, res: Response): Promise<void> {
         try {
-           const id = parseInt(req.params.id) 
-           const user = await userService.getUserById(id)
-           if(user){
-            res.status(200).json(user)
-           }else{
-            res.status(404).json({error:"user not found"})
-           }
+            const id = parseInt(req.params.id)
+            const user = await userService.getUserById(id)
+            if (user) {
+                res.status(200).json(user)
+            } else {
+                res.status(404).json({ error: "user not found" })
+            }
         } catch (error) {
-            console.log(error,"Error in UserController getUserById ");
-            res.status(500).json({error:"Internal Server Error"})
+            console.log(error, "Error in UserController getUserById ");
+            res.status(500).json({ error: "Internal Server Error" })
+        }
+    }
+
+    async createUser(req: Request, res: Response): Promise<void> {
+        try {
+            const { name, email } = req.body as CreateUserDTO
+            const newUser = await userService.createUser({name,email})
+            res.status(201).json(newUser)
+        } catch (error) {
+            console.log(error, "Error in UserController createUser");
+            res.status(400).json({error:"Bad Request"})
         }
     }
 }
+
+
+export default new UserController()
