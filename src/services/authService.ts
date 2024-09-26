332c5f1200b8bd3d9d1c4bca2cc133931452a971
Aswin Reji <aswinreji.004@gmail.com>
2024-09-26 05:50:42 +0000

    feat: JWT signaturing almost done

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -5,12 +5,27 @@ import userRepository from '../repositories/userRepository';
 
 class AuthService {
     async login(email: string, password: string): Promise<string> {
+        const JWT_SECRET = process.env.JWT_SECRET
+        if(!JWT_SECRET){
+            throw new Error("JWT SECRET KEY is not defined!")
+        }
         const user = await userRepository.getUserByEmail(email);
-        if(!user){
+        if (!user) {
             throw new Error("User not found")
         }
 
-        // const isPasswordValid = await bcrypt.compare
-        return "hello"
+        const isPasswordValid = await bcrypt.compare(password, user.password)
+        if (!isPasswordValid) {
+            throw new Error("Invalid Credentials")
+        }
+
+        
+        // Generating jwt token for secure authenctication
+        const token = jwt.sign(
+            { userid: user.userid },JWT_SECRET as jwt.Secret,{expiresIn:"1h"}
+        )
+        return token
     }
 }
+
+export default new AuthService()
