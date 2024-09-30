44ad29d667efc340bd4fa732b8fd53c1b70ae799
Aswin Reji <aswinreji.004@gmail.com>
2024-09-30 12:30:49 +0000

    feat: started user registeration part need to done in authservice first

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,14 +1,29 @@
 import { Request, Response } from 'express';
-import { UserLogin } from '../interfaces/user';
-import authService from '../services/authService';
+import { CreateUserDTO, UserLogin } from '../interfaces/user';
+import AuthService from '../services/authService';
 
 class AuthController {
     async login(req: Request, res: Response): Promise<void> {
         const { email, password } = req.body as UserLogin
 
         try {
-            const token = await authService.login(email,password)
-            res.status(200).json({token})
+            const token = await AuthService.login(email, password)
+            res.status(200).json({ token })
+        } catch (error) {
+            console.log(error, "Auth Controller error");
+            let errorMessage = "Auth Controller Error"
+            if (error instanceof Error) {
+                errorMessage = error.message
+            }
+            res.status(401).json({ error: errorMessage })
+        }
+    }
+
+    async register(req: Request, res: Response): Promise<void> {
+        const { name, email, password } = req.body as CreateUserDTO
+
+        try {
+            
         } catch (error) {
             console.log(error, "Auth Controller error");
             let errorMessage = "Auth Controller Error"
