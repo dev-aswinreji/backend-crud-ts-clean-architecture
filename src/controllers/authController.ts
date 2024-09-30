877f6538370e40d2ebf3c8228db01b462a453155
Aswin Reji <aswinreji.004@gmail.com>
2024-09-30 12:23:13 +0000

    feat: Done auth controller to login the user

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,19 +1,23 @@
 import { Request, Response } from 'express';
 import { UserLogin } from '../interfaces/user';
+import authService from '../services/authService';
 
 class AuthController {
-    async login(req:Request,res:Response):Promise<void>{
-        const {email,password} = req.body as UserLogin
+    async login(req: Request, res: Response): Promise<void> {
+        const { email, password } = req.body as UserLogin
 
         try {
-
+            const token = await authService.login(email,password)
+            res.status(200).json({token})
         } catch (error) {
-            console.log(error,"Auth Controller error");
+            console.log(error, "Auth Controller error");
             let errorMessage = "Auth Controller Error"
-            if(error instanceof Error){
+            if (error instanceof Error) {
                 errorMessage = error.message
             }
-            res.status(401).json({error:errorMessage})
+            res.status(401).json({ error: errorMessage })
         }
     }
 }
+
+export default new AuthController()
