be9a77334e8bc6059261c96225de626977d2b540
Aswin Reji <aswinreji.004@gmail.com>
2024-09-30 13:31:25 +0000

    feat: Added register in auth service

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -2,11 +2,12 @@ import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 
 import userRepository from '../repositories/userRepository';
+import { IUser } from '../interfaces/user';
 
 class AuthService {
     async login(email: string, password: string): Promise<string> {
         const JWT_SECRET = process.env.JWT_SECRET
-        if(!JWT_SECRET){
+        if (!JWT_SECRET) {
             throw new Error("JWT SECRET KEY is not defined!")
         }
         const user = await userRepository.getUserByEmail(email);
@@ -19,12 +20,20 @@ class AuthService {
             throw new Error("Invalid Credentials")
         }
 
-        
+
         // Generating jwt token for secure authenctication
         const token = jwt.sign(
-            { userid: user.userid },JWT_SECRET as jwt.Secret,{expiresIn:"1h"}
+            { userid: user.userid }, JWT_SECRET as jwt.Secret, { expiresIn: "1h" }
         )
         return token
+    }
+
+    async register(user: IUser): Promise<IUser> {
+        const hashedPassword = await bcrypt.hash(user.password,10);
+        return userRepository.createUser({
+            ...user,
+            password:hashedPassword
+        })
     }
 }
 
