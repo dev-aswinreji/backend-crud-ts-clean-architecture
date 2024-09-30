41c9840ecd034cc44c193abf90375c4ce9a8c546
Aswin Reji <aswinreji.004@gmail.com>
2024-09-30 12:15:04 +0000

    feat: Added validation for name email and password need to make it as proper

--- src/utils/validationMiddleWare.ts
+++ src/utils/validationMiddleWare.ts
@@ -1,16 +1,27 @@
 
+import { error } from 'console';
 import { Request, Response, NextFunction } from 'express';
+import { CreateUserDTO } from '../interfaces/user';
 
 const emailRegex = /^[^\s@]+[^\s@]+\.[^\s@]+$/
 
-const validateEmail = (req: Request, res: Response, next: NextFunction) => {
-    const { email } = req.body
+const validation = (req: Request, res: Response, next: NextFunction) => {
+    const { email, name, password } = req.body as CreateUserDTO
+
+    if (!name) {
+        return res.status(400).json({error:"Invalid name"})
+    }
 
     if (!email || !emailRegex.test(email)) {
         return res.status(400).json({ error: "Invalid email format" })
     }
+
+    if(!password){
+        return res.status(400).json({error:"Invalid password"})
+    }
+
     next()
 }
 
-export default validateEmail;
+export default validation;
 
