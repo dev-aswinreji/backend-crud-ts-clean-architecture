19e22c3b1855cb0926ca5ef65544ec738ae115a3
Aswin Reji <aswinreji.004@gmail.com>
2024-10-03 18:51:54 +0000

    feat: Added route for user proper registration and login

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,11 +1,14 @@
 
 import { Router } from "express";
 import userControllers from "../controllers/userControllers";
+import authController from "../controllers/authController";
 
 const router = Router()
 
 router.get('/users', userControllers.getAllUsers.bind(userControllers))
 router.get('/users/:id', userControllers.getUserById.bind(userControllers))
 router.post('/users', userControllers.createUser.bind(userControllers))
+router.post('/login',authController.login.bind(authController))
+router.post('/register',authController.register.bind(authController))
 
 export default router
